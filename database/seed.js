cd7e9bc02897f767107ec6440257660bdc45a85c
MD2020m <dmarshall2019@gmail.com>
2025-12-07 15:27:43 -0600

    Write basic GET routes for Campaigns and PlayerCharacters; fix bug in seed.js

--- database/seed.js
+++ database/seed.js
@@ -33,19 +33,19 @@ async function seedDatabase() {
                 title: "Hoard of the Dragon Queen",
                 description: "Track down an ancient and malevolent dragon to stop her rampage and steal the treasure she hoards",
                 schedule: "Thursdays, 7-10pm",
-                userId: users[1].id
+                userId: users[1].userId
             },
             {
                 title: "High Seas Heist",
                 description: "Sail the seven seas collecting treasure along the way in search of a legendary quarry fabled to lie under heavy guard in a far off land",
                 schedule: null,
-                userId: users[1].id
+                userId: users[1].userId
             },
             {
                 title: "A New World",
                 description: "A beam of light appears from above in a fading old world underground. A group of heros are sent on an expedition in search of a new hope at its source",
                 schedule: null,
-                userId: users[0].id
+                userId: users[0].userId
             }
         ]);
 
@@ -75,8 +75,8 @@ async function seedDatabase() {
                 backstory: 'A banished princess from a far off land, Alyson Woodbrace learned to make her way through the world with her sword. Though a seasoned traveler and an excellent warrior, she has had little time to spare on much else',
                 alignment:'True Neutral',
                 origin: 'Royal',
-                userId: users[0].id,
-                campaignId: campaigns[0].id
+                userId: users[0].userId,
+                campaignId: campaigns[0].campaignId
             },
             {
                 alive: true,
@@ -102,8 +102,8 @@ async function seedDatabase() {
                 backstory: "In Gorub's tribe, orcs are sent out to prove themselves by using their strength and whatever cunning they can muster to bring a great treasure back to the tribe. Gorub has been searching for years now, and is determined to end his search soon",
                 alignment: 'Chaotic Neutral',
                 origin: null,
-                userId: users[2].id,
-                campaignId: campaigns[1].id
+                userId: users[2].userId,
+                campaignId: campaigns[1].campaignId
             },
             {
                 alive: true,
@@ -129,8 +129,8 @@ async function seedDatabase() {
                 backstory: 'Poor Gruff was kidnapped at a young age by pirates who believed a minotaur might prove useful and raised on the seas by a skilled dwarf who shared his fate. When the crew that had conscripted him was defeated, Gruff narrowly escaped with his life and a determination to find his way home',
                 alignment: 'Neutral Good',
                 origin: null,
-                userId: users[0].id,
-                campaignId: campaigns[1].id
+                userId: users[0].userId,
+                campaignId: campaigns[1].campaignId
             },
             {
                 alive: true,
@@ -156,8 +156,8 @@ async function seedDatabase() {
                 backstory: "Burny was created from scavenged tree roots as an eternal sentinel for the capital during the kingdom's golden age. But that age has gone now, and Burny has guarded only himself for some time, having been discarded by the capital's remaining citizens, who would prefer no sentinel keep watch over them",
                 alignment: 'lawful good',
                 origin: null,
-                userId: users[1].id,
-                campaignId: campaigns[2].id
+                userId: users[1].userId,
+                campaignId: campaigns[2].campaignId
             }
         ]);
 
@@ -165,15 +165,15 @@ async function seedDatabase() {
         campaignNoteThreads = await CampaignNoteThread.bulkCreate([
             {
                 toCharacter: null,
-                campaignId: campaigns[0].id
+                campaignId: campaigns[0].campaignId
             },
             {
-                toCharacter: playerCharacters[0].id,
+                toCharacter: playerCharacters[0].charId,
                 campaignId: playerCharacters[0].campaignId
             },
             {
                 toCharacter: null,
-                campaignId: campaigns[1].id
+                campaignId: campaigns[1].campaignId
             }
         ]);
 
@@ -182,20 +182,20 @@ async function seedDatabase() {
             {
                 content: "Hey everyone, here's an summary in case you forgot anything from last night's campaign...",
                 timePosted: Sequelize.literal('CURRENT_TIMESTAMP'),
-                userId: users[1].id,
-                threadId: campaignNoteThreads[0].id
+                userId: users[1].userId,
+                threadId: campaignNoteThreads[0].threadId
             },
             {
                 content: "Hey, during the long rest you took at the end of last session, your character had a dream about... ",
                 timePosted: Sequelize.literal('CURRENT_TIMESTAMP'),
-                userId: users[1].id,
-                threadId: campaignNoteThreads[1].id
+                userId: users[1].userId,
+                threadId: campaignNoteThreads[1].threadId
             },
             {
                 content: "Hey everyone, here's a quick preface for what's been going on before our first session in the world of High Seas Heist",
                 timePosted: Sequelize.literal('CURRENT_TIMESTAMP'),
-                userId: users[1].id,
-                threadId: campaignNoteThreads[2].id
+                userId: users[1].userId,
+                threadId: campaignNoteThreads[2].threadId
             }
         ]);
 
@@ -204,14 +204,14 @@ async function seedDatabase() {
             {
                 content: "Hey everyone, I was hoping we could switch to a new day for session. Thursday doesn't work for me anymore",
                 timePosted: Sequelize.literal('CURRENT_TIMESTAMP'),
-                campaignId: campaigns[0].id,
-                userId: users[0].id
+                campaignId: campaigns[0].campaignId,
+                userId: users[0].userId
             },
             {
                 content: "Hey everyone, I'm really excited to start 'A New World'! Can you all let me know what days and times work best for regular session?",
                 timePosted: Sequelize.literal('CURRENT_TIMESTAMP'),
-                campaignId: campaigns[2].id,
-                userId: users[0].id
+                campaignId: campaigns[2].campaignId,
+                userId: users[0].userId
             }
         ]);
 
--- server.js
+++ server.js
@@ -29,5 +29,113 @@ testConnection();
 
 
 
-// USER ROUTES 
+// CAMPAIGN ROUTES 
 
+// TODO: Adjust this endpoint to return only campaigns a user is involved in
+// GET /api/campaigns - Return all campaigns
+
+// TODO: Get endpoint to return info about players and characters involved
+app.get('/api/campaigns', /*requireAuth,*/ async (req, res) => {
+    try {
+        const campaigns = await Campaign.findAll({
+            include: [
+                {
+                    model: User,
+                    as: 'dungeonMaster',
+                    attributes: ['userId','username']
+                },
+                {
+                    model: PlayerCharacter,
+                    attributes: ['charId','alive','name','race','gender'],
+                    include: [
+                        {
+                            model: User,
+                            attributes: ['userId','username']
+                        }
+                    ]
+                }
+            ]
+        });
+
+        res.json(campaigns);
+    } catch (error) {
+        console.error('Error fetching campaigns:', error);
+        res.status(500).json({ error: 'Failed to fetch campaigns' });
+    }
+});
+
+// TODO: Adjust this endpoint to require authorization to view the campaign
+// GET /api/campaing/:id - Return a Campaign by id
+
+// TODO: Get endpoint to return info about characters and players involved
+app.get('/api/campaigns/:id', /*requireAuth,*/ async (req, res) => {
+    try {
+        const campaign = await Campaign.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    as: 'dungeonMaster',
+                    attributes: ['userId','username']
+                },
+                {
+                    model: PlayerCharacter,
+                    attributes: ['charId','alive','name','race','gender'],
+                    include: [
+                        {
+                            model: User,
+                            attributes: ['userId','username']
+                        }
+                    ]
+                }
+            ]
+        })
+
+        if (!campaign) {
+            return res.status(404).json({ error: 'Campaign not found' });
+        }
+
+        res.json(campaign);
+    } catch (error) {
+        console.error('Error fetching campaign:', error);
+        res.status(500).json({ error: 'Failed to fetch campaign' });
+    }
+});
+
+
+// PLAYERCHARACTER ROUTES 
+
+// GET /api/my_characters - Currently returns all characters (Later to return only a User's characters)
+// TODO: adjust endpoint to require authentication
+app.get('/api/my_characters', /*requireAuth,*/ async (req, res) => {
+    try{
+        const chars = await PlayerCharacter.findAll({});
+
+        res.json(chars);
+    } catch (error) {
+        console.error('Error fetching player characters:', error);
+        res.status(500).json({ error: 'Failed to fetch player characters' });
+    }
+});
+
+app.get('/api/my_characters/:id', /*requireAuth,*/ async (req, res) => {
+    try {
+        const char = await PlayerCharacter.findByPk(req.params.id);
+
+        if (!char) {
+            return res.status(404).json({ error: 'Player Character not found' });
+        }
+
+        res.json(char);
+    } catch (error) {
+        console.error('Error fetching Player Character:', error);
+        res.status(500).json( {error: 'Failed to fetch character' });
+    }
+});
+
+
+
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+})
